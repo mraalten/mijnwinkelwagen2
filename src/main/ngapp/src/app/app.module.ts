9c92ae0dfa53959ce3dec87a89137542e48538cf
Aalten <richard.aalten@rabobank.nl>
2017-09-10 22:21:21 +0200

    Winkelwagen app working with static js/html and css. No rest endpoint yet

--- src/main/ngapp/src/app/app.module.ts
+++ src/main/ngapp/src/app/app.module.ts
@@ -4,6 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {Routes, RouterModule} from "@angular/router";
+import {MijnWinkelwagenModule} from "./mijnwinkelwagen/mijnwinkelwagen.module";
+
+const appRoutes: Routes = [
+  { path: '',
+    redirectTo: '/mijnwinkelwagen',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +21,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MijnWinkelwagenModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
